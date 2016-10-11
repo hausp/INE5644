fafca27d792a6fe567c463a6086650154c6c14a2
Ghabriel <ghabriel.nunes@hotmail.com>
2016-10-11 11:20:51 -0300

    Added a file with histogram commands

---
+++ hists.R
@@ -0,0 +1,35 @@
+bar = function(data, title) {
+	c = table(data);
+	barplot(c / sum(c), main=title);
+}
+
+# HasName
+hist(unlist(data$HasName));
+
+# DateTime
+hist(unlist(data$DateTime));
+
+# OutcomeType
+levels(data$OutcomeType) = outcomeTypes;
+levels(data$OutcomeType)[4] = "Ret_owner";
+bar(data$OutcomeType, "Outcome Type");
+
+# AnimalType
+levels(data$AnimalType) = animalTypes;
+bar(data$AnimalType, "Animal Type");
+
+# SexUponOutcome
+levels(data$SexUponOutcome) = genders;
+levels(data$SexUponOutcome)[4] = "Neut. Male";
+levels(data$SexUponOutcome)[5] = "Spayed Fem";
+bar(data$SexUponOutcome, "Sex upon outcome");
+
+# DaysUponOutcome
+hist(data$DaysUponOutcome);
+
+# IsMixedBreed
+bar(unlist(data$IsMixedBreed), "Is mixed breed");
+
+# IsMixedColor
+bar(unlist(data$IsMixedColor), "Is mixed color");
+
